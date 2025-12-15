048239daaedc93ae2d273e3c5fd3e1182331e811
Igor Work <keaerta@gmail.com>
2025-12-15 17:16:16 +0500

    chore(gitignore): add Docker ignored files

---
+++ src/main/java/org/file/cloud/Pet6Application.java
@@ -0,0 +1,13 @@
+package org.file.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pet6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pet6Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/file/cloud/Pet6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.file.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pet6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
