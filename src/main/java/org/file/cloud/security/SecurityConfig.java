62b6c65973f0d106e1bb63fba353821d9301e47e
Igor_home <keaerta@gmail>
2025-12-29 18:49:34 +0500

    feat(SecurityConfig): add unauthorizedHandler with JSON 401 response

--- src/main/java/org/file/cloud/security/SecurityConfig.java
+++ src/main/java/org/file/cloud/security/SecurityConfig.java
@@ -1,8 +1,13 @@
 package org.file.cloud.security;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,11 +16,13 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.context.SecurityContextRepository;
 
+@Slf4j
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
@@ -34,6 +41,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 )
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler()))
                 .logout(logout -> logout.logoutUrl("/api/auth/sign-out")
                         .logoutSuccessHandler(logoutHandler));
 
@@ -53,5 +61,17 @@ public class SecurityConfig {
     @Bean
     public SecurityContextRepository securityContextRepository() {
         return new HttpSessionSecurityContextRepository();
+    }
+
+    @Bean
+    public AuthenticationEntryPoint unauthorizedHandler() {
+        return ((request, response, authException) -> {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().write("""
+                    {"message": "Authentication is required"}
+                    """);
+            log.warn("Unauthorized access attempt: path = {}, reason = {}", request.getRequestURI(), authException.getMessage());
+        });
     }
 }
