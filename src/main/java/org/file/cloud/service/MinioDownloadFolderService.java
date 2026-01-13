f936640253ef37f044e58be629461e5b7083a68d
Igor Work <keaerta@gmail.com>
2026-01-13 11:26:20 +0500

    feat(downloadFolder): add support for empty folder entries

--- src/main/java/org/file/cloud/service/MinioDownloadFolderService.java
+++ src/main/java/org/file/cloud/service/MinioDownloadFolderService.java
@@ -20,55 +20,34 @@ import java.util.zip.ZipOutputStream;
 @Service
 @RequiredArgsConstructor
 public class MinioDownloadFolderService {
-    private final MinioClient minioClient;
     private final MinioStorageService minioStorageService;
 
-    private final String MAIN_BUCKET = "user-files";
-    private final String USER_ROOT_FOLDER_TEMPLATE = "user-%s-files/";
-
-//    public void downloadFolder(String username, String resourcePath, OutputStream outputStream) {
-        public void downloadFolder(String fullPath, OutputStream outputStream) {
-
-//        String fullPath = userRootFolder + resourcePath;
-
-
-        // если заканчивается на "/"
-        // получаю список объектов внутри папки
-        try {
-            ZipOutputStream zipOutputStream = new ZipOutputStream(outputStream);
-
-//            Iterable<Result<Item>> results = minioClient.listObjects(
-//                    ListObjectsArgs.builder()
-//                            .bucket(MAIN_BUCKET)
-//                            .prefix(fullPath)
-//                            .recursive(true)
-//                            .build());
+    public void downloadFolder(String fullPath, OutputStream outputStream) {
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(outputStream)) {
 
             Iterable<Result<Item>> results = minioStorageService.getObjects(fullPath);
 
             // если пустая папка - пропускаю
             for (Result<Item> result : results) {
                 Item item = result.get();
-                if (item.isDir()) continue;
+                String objectName = item.objectName();
+                if (item.isDir()) {
+                    zipOutputStream.putNextEntry(new ZipEntry(objectName + "/"));
+                    zipOutputStream.closeEntry();
+                    log.info("Adding empty folder Entry name: {} ", objectName);
+                    continue;
+                }
 
                 // определяю имя для зип файла
-                String objectName = item.objectName();
                 log.info("objectName: {}", objectName);
                 String zipEntryName = objectName.substring(fullPath.length());
-                log.info("Adding zip Entry name: {} ", zipEntryName);
+                log.info("Adding file Entry name: {} ", zipEntryName);
 
                 // записываю энтри в зип поток
                 zipOutputStream.putNextEntry(new ZipEntry(zipEntryName));
 
                 // получаю каждый объект в виде потока и передаю в зип поток
-                try (
-//                        InputStream stream = minioClient.getObject(
-//                        GetObjectArgs.builder()
-//                                .bucket(MAIN_BUCKET)
-//                                .object(objectName)
-//                                .build())
-                        InputStream stream = minioStorageService.getObjectStream(objectName);
-                ) {
+                try (InputStream stream = minioStorageService.getObjectStream(objectName)) {
                     stream.transferTo(zipOutputStream);
                 }
                 zipOutputStream.closeEntry();
