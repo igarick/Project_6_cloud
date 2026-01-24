d78c1349bfbc69410ae510062da0e81c9a407e91
Igor_home <keaerta@gmail>
2026-01-24 15:03:37 +0500

    feat(swagger): add Swagger annotations for UserController

--- src/main/java/org/file/cloud/controller/UserController.java
+++ src/main/java/org/file/cloud/controller/UserController.java
@@ -1,6 +1,7 @@
 package org.file.cloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.file.cloud.controller.swagger.UserSwagger;
 import org.file.cloud.dto.UsernameDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,7 +15,7 @@ import static org.springframework.http.HttpStatus.OK;
 @Slf4j
 @RestController
 @RequestMapping("/api/user")
-public class UserController {
+public class UserController implements UserSwagger {
 
     @GetMapping("/me")
     public ResponseEntity<UsernameDto> getUser(@AuthenticationPrincipal UserDetails userDetails) {
---
+++ src/main/java/org/file/cloud/controller/swagger/UserSwagger.java
@@ -0,0 +1,38 @@
+package org.file.cloud.controller.swagger;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.file.cloud.dto.ErrorMessageDto;
+import org.file.cloud.dto.UsernameDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Tag(name = "Current user")
+public interface UserSwagger {
+    @Operation(
+            summary = "Get current user info",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "User info retrieved successfully",
+                            content = @Content(schema = @Schema(implementation = UsernameDto.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "User is not authorized",
+                            content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "Internal server error",
+                            content = @Content(schema = @Schema(implementation = ErrorMessageDto.class))
+                    )
+            }
+
+    )
+    public ResponseEntity<UsernameDto> getUser(@AuthenticationPrincipal UserDetails userDetails);
+}
